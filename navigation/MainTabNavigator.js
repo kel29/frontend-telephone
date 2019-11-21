b5e332b2815e28f379c451d5abc98fb6a4ad0204
Krista Loven <krista.loven.87@gmail.com>
2019-11-21 14:23:05 -0800

    created rules screen

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Platform } from 'react-native'
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import LinksScreen from '../screens/LinksScreen'
+import RulesScreen from '../screens/RulesScreen'
+import SettingsScreen from '../screens/SettingsScreen'
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {}
+})
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen
+  },
+  config
+)
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  )
+}
+
+HomeStack.path = ''
+
+const LinksStack = createStackNavigator(
+  {
+    Links: LinksScreen
+  },
+  config
+)
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Links',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
+  )
+}
+
+LinksStack.path = ''
+
+const RulesStack = createStackNavigator(
+  {
+    Rules: RulesScreen
+  },
+  config
+)
+
+RulesStack.navigationOptions = {
+  tabBarLabel: 'Rules',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
+  )
+}
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen
+  },
+  config
+)
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  )
+}
+
+SettingsStack.path = ''
+
+const tabNavigator = createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  RulesStack,
+  SettingsStack
+})
+
+tabNavigator.path = ''
+
+export default tabNavigator
---
+++ screens/RulesScreen.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { ScrollView, StyleSheet, Text } from 'react-native'
+
+function RulesScreen () {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.overview}>
+        Welcome to Telephone! Similar to the classic childrens
+        game, we're banking on a premise of hilarity, miscomunication
+        and confusion. It will be a grand ole time!
+      </Text>
+      <Text style={styles.tldr}>
+        TLDR: Player one writes a sentence, player two attempts
+        to draw it. Player three writes a sentence to describe
+        player two's drawing, and the game continues to alternate
+        between describing and drawing until you choose to end it.
+        Then, you get to have a lovely laugh over evolution of what
+        just happened.
+      </Text>
+      <Text style={styles.overview}>
+        The first player will enter a sentence. It should be descriptive,
+        but not insane. Think 'The cow jummped over the spoon', not
+        'The black and white spotted Holstein Friesian cow, who had always
+        dreamed of a career as a ballerina, took a running leap over the
+        Spoonbridge and Cherry sculpture.' I mean, you can always go with
+        something crazy, but I can't promise your friends will ever want to
+        play with you again.
+      </Text>
+      <Text style={styles.overview}>
+        Next, player two will do their very best to sketch out an
+        image representation of that sentence player one so kindly
+        shared. Once done, make sure to hit the next button before
+        passing the phone to the next player. They will only see the
+        sketch just created, and will have to describe it in a sentence.
+        Once done and submitted, they should pass the phone on to the
+        next player, and the game will continue to cycle between drawing
+        and describing until you run out of friends. At that point, hit
+        the end game button and have a laugh reviewing the story that
+        has unfolded.
+      </Text>
+
+    </ScrollView>
+  )
+}
+
+RulesScreen.navigationOptions = {
+  title: 'Rules'
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#fff'
+  },
+  tldr: {
+    flex: 1,
+    textAlign: 'justify',
+    fontWeight: 'bold',
+    paddingTop: 15,
+    paddingBottom: 10
+  },
+  overview: {
+    flex: 1,
+    textAlign: 'justify',
+    paddingTop: 10
+  }
+})
+
+export default RulesScreen
