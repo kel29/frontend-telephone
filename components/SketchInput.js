2eb34d14de149caf4e683dad8021d621b951ee66
Krista Loven <krista.loven.87@gmail.com>
2019-11-25 12:52:18 -0800

    update expo version and dependencies, add expo pixi and start sketch screen logic

--- components/SketchInput.js
+++ components/SketchInput.js
@@ -1,14 +1,54 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import Expo from 'expo'
+import React, { PureComponent } from 'react'
+import { Button, StyleSheet, View } from 'react-native'
+import * as ExpoPixi from 'expo-pixi'
 
-const SketchInput = () => {
-  return (
-    <View>
-      <Text>
-      This will be the drawing canvas
-      </Text>
-    </View>
-  )
+
+class SketchInput extends PureComponent {
+  state = {
+    image: null,
+    strokeColor: Math.random() * 0xffffff,
+    strokeWidth: 14
+  }
+
+  onChangeAsync = async () => {
+    const { uri } = await this.sketch.takeSnapshotAsync()
+
+    this.setState({
+      image: { uri },
+      strokeWidth: 14,
+      strokeColor: Math.random() * 0xffffff,
+    });
+  };
+
+  onReady = () => {
+    console.log('ready!');
+  };
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <ExpoPixi.Sketch
+          ref={ref => (this.sketch = ref)}
+          style={styles.sketch}
+          strokeColor={this.state.strokeColor}
+          strokeWidth={this.state.strokeWidth}
+          strokeAlpha={1}
+          onChange={this.onChangeAsync}
+          onReady={this.onReady}
+        />
+      </View>
+    )
+  }
 }
 
 export default SketchInput
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  sketch: {
+    flex: 1
+  }
+})
