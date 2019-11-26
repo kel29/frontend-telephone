04fc9f2ac64a92d689ef38394ed64b0f793709a5
Krista Loven <krista.loven.87@gmail.com>
2019-11-25 17:41:21 -0800

    move sketching canvas to SketchScreen

--- screens/SketchScreen.js
+++ screens/SketchScreen.js
@@ -1,34 +1,62 @@
-import React from 'react'
-import { View, StyleSheet, Button, Text } from 'react-native'
+import React, { PureComponent } from 'react'
+import { View, StyleSheet, Button } from 'react-native'
 import SentenceDisplay from '../components/SentenceDisplay'
-import SketchInput from '../components/SketchInput' // TODO: hook up drawing canvas
+import { captureRef as takeSnapshotAsync } from 'react-native-view-shot'
+import { Sketch } from 'expo-pixi'
 // TODO: Refractor to incorporate:
 // import EndGameButton from '../components/EndGameButton'
 // import NextPlayerButton from '../components/NextPlayerButton'
 
-const SketchScreen = (props) => {
-  const gameRounds = props.navigation.getParam('game_rounds')
-  const id = props.navigation.getParam('id')
-
-  const navigateToSentence = () => {
-    // TODO: edit gameRounds to push in the newly created image before navigating
-    props.navigation.navigate('Sentence', { id: id, game_rounds: gameRounds })
+class SketchScreen extends PureComponent {
+  state = {
+    image: null,
+    strokeColor: '#000',
+    strokeWidth: 14
   }
 
-  const endGame = () => {
+  gameRounds = this.props.navigation.getParam('game_rounds')
+  id = this.props.navigation.getParam('id')
+
+  draw = async () => {
+    const { uri } = await this.sketch.takeSnapshotAsync()
+    console.log({ uri })
+
+    this.setState({
+      image: { uri },
+      strokeWidth: 14,
+      strokeColor: '#000'
+    })
+  }
+
+  navigateToSentence = () => {
+    // TODO: edit gameRounds to push in the newly created image before navigating
+    this.props.navigation.navigate('Sentence', { id: this.id, game_rounds: this.gameRounds })
+  }
+
+  endGame = () => {
     // TODO: edit gameRounds to push in the newly created image before navigating
     // TODO: post the rounds to the database
-    props.navigation.navigate('Display', { id: id, game_rounds: gameRounds })
+    this.props.navigation.navigate('Display', { id: this.id, game_rounds: this.gameRounds })
   }
 
-  return (
-    <View style={styles.container}>
-      <SentenceDisplay sentence={gameRounds[gameRounds.length - 1].sentence} />
-      <SketchInput />
-      <Button title='End Game' onPress={endGame} />
-      <Button title='Submit' onPress={navigateToSentence} />
-    </View>
-  )
+  render () {
+    return (
+      <View style={styles.container}>
+        <SentenceDisplay sentence={this.gameRounds[this.gameRounds.length - 1].sentence} />
+        <Sketch
+          ref={ref => (this.sketch = ref)}
+          style={styles.sketch}
+          strokeColor={this.state.strokeColor}
+          strokeWidth={this.state.strokeWidth}
+          strokeAlpha={1}
+          onChange={this.draw}
+        />
+        <Button title='Undo' onPress={() => this.sketch.undo()} />
+        <Button title='End Game' onPress={this.endGame} />
+        <Button title='Submit' onPress={this.navigateToSentence} />
+      </View>
+    )
+  }
 }
 
 export default SketchScreen
@@ -38,5 +66,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 15,
     marginTop: 30
+  },sketch: {
+    flex: 1
   }
 })
