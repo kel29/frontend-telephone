5126160f75e74e779e9b59055172a22e8dee3626
Krista Loven <krista.loven.87@gmail.com>
2020-01-08 19:57:45 -0800

    Format past games titles

--- src/components/GameDisplay.js
+++ src/components/GameDisplay.js
@@ -10,12 +10,21 @@ import {
 import Styles from '../constants/Style'
 
 const GameList = (props) => {
+  const showGameTitle = () => {
+    const title = props.game.game_rounds[0].sentence.trim()
+    if (title.length > 40) {
+      return `${title.slice(0, 40).trim()}...`
+    } else {
+      return title
+    }
+  }
+
   return (
     <ListItem>
       <Left>
         <Button transparent onPress={() => props.handleViewGame(props.game)}>
           <Text style={Styles.buttonText}>
-            {props.game.game_rounds[0].sentence}
+            {showGameTitle()}
           </Text>
         </Button>
       </Left>
